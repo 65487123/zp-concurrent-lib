bcb3ae6ae4d4e0d6fa64fb2784601aaaf51c491c
Lu Zeping <zeping.lu@utstar.com>
2020-11-12 15:48:14 +0800

    update

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -5,6 +5,7 @@ import com.lzp.util.concurrent.threadpool.ThreadFactoryImpl;
 import com.lzp.util.concurrent.threadpool.ThreadPoolExecutor;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Description:
@@ -13,28 +14,29 @@ import java.util.concurrent.*;
  * @date: 2020/8/9 11:46
  */
 public class Test {
-    static int a ;
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService executorService  = new ThreadPoolExecutor(4,4,0,new ArrayBlockingQueue(50000000),new ThreadFactoryImpl("test"));
-        CountDownLatch countDownLatch = new CountDownLatch(100000000);
+    public static void main(String[] args) throws InterruptedException{
+        ExecutorService executorService  = new ThreadPoolExecutor(4,4,0,new LinkedBlockingQueue(),new ThreadFactoryImpl("test"));
+        CountDownLatch countDownLatch = new CountDownLatch(5000000);
         Runnable runnable = () -> {
             sum();
             countDownLatch.countDown();
         };
         long now = System.currentTimeMillis();
-        for (int i = 0 ;i <100000000 ;i++ ) {
+        for (int i = 0 ;i <5000000 ;i++ ) {
             executorService.execute(runnable);
         }
         countDownLatch.await();
         System.out.println(System.currentTimeMillis() - now);
-
     }
-
     static int sum() {
         int a = 0;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             a += i;
         }
         return a;
+    }
+
+    static void test() throws InterruptedException {
+
     }
 }
